59dffcafd231985d069533d4034d1f152a8f0415
adamuso <adamuso213@gmail.com>
2022-10-23 14:39:47 +0200

    Dataset partly complete, the merged positions and intensities file not yet implemented. (Loading just positions for now).

--- Dev/data_preprocess.py
+++ Dev/data_preprocess.py
@@ -21,3 +21,16 @@ def load_positions(path):
     img = img.reshape(shape[0] * shape[1], shape[2])
     # return the image
     return img
+
+def load_intensities(path):
+    """TODO Documentation Required"""
+    # read the exr file
+    img = cv.imread(str(path), cv.IMREAD_ANYCOLOR | cv.IMREAD_ANYDEPTH)
+    # convert to float32
+    img = img.astype(np.float32)
+    # get the shape of the image
+    shape = img.shape
+    # reshape the image to a 2d array
+    img = img.reshape(shape[0] * shape[1], shape[2])
+    # return the image
+    return img
