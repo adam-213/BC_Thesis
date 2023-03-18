3f59d5c1972d77e9ba4766cb646a30eb7cee8db3
adamuso <adamuso213@gmail.com>
2023-03-18 21:53:06 +0100

    doesnt work just backup

--- c_TM_CNN.py
+++ c_TM_CNN.py
@@ -4,46 +4,80 @@ import torch.nn.functional as F
 
 
 class Attention(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, instances):
         super(Attention, self).__init__()
 
-        self.conv_mask = nn.Conv2d(1, out_channels, kernel_size=1, stride=1, padding=0)
+        self.conv_mask = nn.Conv2d(instances, out_channels * instances, kernel_size=1, stride=1, padding=0,
+                                   groups=instances)
         self.conv_feat = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
         self.conv_out = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, padding=0)
         self.gamma = nn.Parameter(torch.zeros(1))
+        self.out_channels = out_channels
 
-    def forward(self, x, mask):
-        mask = self.conv_mask(mask)
+    def forward(self, x, masks):
+        batch_size, instances, _, _ = masks.size()
         feat = self.conv_feat(x)
+        out = torch.zeros_like(feat)
 
-        mask = F.softmax(mask, dim=-1)
-        feat = feat * mask
+        masks = self.conv_mask(masks)
+        masks = F.softmax(masks.view(batch_size, instances, -1), dim=-1).view_as(masks)
 
-        feat = self.conv_out(feat)
-        feat = self.gamma * feat + x
+        for i in range(instances):
+            mask = masks[:, i * self.out_channels:(i + 1) * self.out_channels, :, :]
+            instance_feat = feat * mask
+            instance_feat = self.conv_out(instance_feat)
+            out += instance_feat
 
-        return feat
+        out = self.gamma * out + x
+        return out
 
 
 class TM_CNN(nn.Module):
-    def __init__(self, in_channels, attention_channels):
+    def __init__(self, in_channels, attention_channels, instances):
         super(TM_CNN, self).__init__()
-        # +1 for mask channel
-        self.conv1 = nn.Conv2d(in_channels + 1, 64, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
-        self.attention = Attention(256, attention_channels)
-        self.fc1 = nn.Linear(256 * 8 * 8, 1024)
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 16)
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.attention = Attention(64, 64, instances)
+        self.fc1 = nn.Linear(792576, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 16)
+        self.maxpoll = nn.MaxPool2d(kernel_size=2, stride=2)
 
-    def forward(self, x, mask):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.attention(x, mask)
-        x = x.view(-1, 256 * 8 * 8)
-        x = self.fc1(x)
-        x = self.fc2(x)
+    def forward(self, x, masks):
+        masks = self.reshape_masks(masks)  # reshape to 1/4 of the original resolution
+        x = F.relu(self.conv1(x))
+        x = self.maxpoll(x)
+        x = F.relu(self.conv2(x))
+        x = self.maxpoll(x)
+        x = F.relu(self.conv3(x))
+        x = self.maxpoll(x)
+        x = self.attention(x, masks)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+    def reshape_masks(self, masks):
+        # reshape to 1/4 of the original resolution
+        batch_size, instances, height, width = masks.size()
+        masks = F.interpolate(masks, size=(height // 8, width // 8), mode='nearest')
+        return masks
+
+    def loss(self, pred, gt):
+        # reshape
+        pred = pred.view(-1, 4, 4)  # (batch_size, 4, 4)
+        # split the matrix into rotation and translation
+        pred_r = pred[:, :3, :3]
+        pred_t = pred[:, :3, 3]
+        gt_r = gt[:, :3, :3]
+        gt_t = gt[:, :3, 3]
+
+        # compute the loss
+        # translation loss is just classic mse
+        loss_t = F.mse_loss(pred_t, gt_t)
+        # rotation loss is geodesic distance + frobenius norm
+        loss_r = torch.norm(pred_r - gt_r, p='fro', dim=(1, 2)) + torch.norm(pred_r - gt_r, p=2, dim=(1, 2))
+        loss_r = torch.mean(loss_r)
+        return loss_t + loss_r
--- d_TM_CNN_Training.py
+++ d_TM_CNN_Training.py
@@ -15,72 +15,71 @@ from b_DataLoader_RCNN import createDataLoader
 
 
 class Trainer:
-    def __init__(self, model, train_loader, val_loader, criterion, optimizer, device, scaler):
+    def __init__(self, model, train_loader, val_loader, optimizer, device, scaler):
         self.model = model
         self.train_loader = train_loader
         self.val_loader = val_loader
-        self.criterion = criterion
         self.optimizer = optimizer
         self.device = device
         self.scaler = scaler
 
-    def train(self, epochs, mini_batch_size):
+    def train(self, epochs):
         for epoch in range(epochs):
             running_loss = 0.0
-            # batchsize must be 1
             for i, (images, masks, tms) in enumerate(self.train_loader):
-                size = max(1, masks.size()[0] // mini_batch_size)
-                for j in range(size):
-                    # use torch to split the data into mini-batches
-                    cut_size = min(mini_batch_size, masks.size()[0] - j * mini_batch_size)
-                    masks_mini = masks[j * cut_size:(j + 1) * cut_size]
-                    tms_mini = tms[j * cut_size:(j + 1) * cut_size]
+                # split the batch into mini-batches
+                images = images.to(self.device)
+                masks = masks.to(self.device)
+                tms = tms.to(self.device)
 
-                    # concat mask to the image on channel dimension for each image in mini-batch
-                    # and stack them into the batch dimension
-
-                    # create a temp list to store the batch
-                    temp = []
-                    for mask in masks_mini:
-                        image = torch.cat((images.squeeze(), mask.unsqueeze(0)), dim=0)
-                        temp.append(image)
-
-                    # stack the batch
-                    # should give me a tensor of size (mini_batch_size, imagechannles + 1, w,h)
-                    images_mini = torch.stack(temp, dim=0)
-
-                    images_mini = images_mini.to(self.device)
-                    tms_mini = tms_mini.to(self.device)
-                    assert images_mini.size()[0] == cut_size
-                    assert tms_mini.size()[0] == cut_size
-
+                for minimini_batch in range(0, images.shape[0], 2):
                     # zero the parameter gradients
                     self.optimizer.zero_grad()
 
                     with autocast():
                         # forward + backward + optimize
-                        outputs = self.model(images_mini)
-                        loss = self.criterion(outputs, tms_mini)
+                        outputs = self.model(images[minimini_batch:minimini_batch + 2],
+                                             masks[minimini_batch:minimini_batch + 2])
+                        print("stop")
+                        loss = self.model.loss(outputs, tms[minimini_batch:minimini_batch + 2])
 
                     self.scaler.scale(loss).backward()
                     self.scaler.step(self.optimizer)
                     self.scaler.update()
+
+                    # print statistics
+                    running_loss += loss.item()
+                    if i % 10 == 9:
+                        print('[%d, %5d] loss: %.3f' %
+                              (epoch + 1, i + 1, running_loss / 10))
+                        running_loss = 0.0
+
+                # # zero the parameter gradients
+                # self.optimizer.zero_grad()
+                #
+                # with autocast():
+                #     # forward + backward + optimize
+                #     outputs = self.model(images, masks)
+                #     loss = self.criterion(outputs, tms)
+                #
+                # self.scaler.scale(loss).backward()
+                # self.scaler.step(self.optimizer)
+                # self.scaler.update()
 
 
 if __name__ == "__main__":
     base_path = pathlib.Path(__file__).parent.absolute()
     coco_path = base_path.joinpath('COCO_TEST')
     channels = [0, 1, 2, 3, 4, 5, 9]
-
+    instances = 1
     train_dataloader, val_dataloader, stats = createDataLoader(coco_path, 1, channels=channels,
-                                                               model="tm")
+                                                               model="tm", instances=instances)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model = TM_CNN(in_channels=len(channels), attention_channels=64).to(device)
-    criterion = nn.CrossEntropyLoss()
+    model = TM_CNN(in_channels=len(channels), attention_channels=32, instances=instances).to(device)
     optimizer = Adam(model.parameters(), lr=0.001)
     scaler = GradScaler()
 
-    trainer = Trainer(model, train_dataloader, val_dataloader, criterion, optimizer, device, scaler)
-    trainer.train(epochs=50, mini_batch_size=16)
+    trainer = Trainer(model, train_dataloader, val_dataloader, optimizer, device, scaler)
+    trainer.train(epochs=50)
